ee901c9e638fa94401176648613db4c30dacb800
jeboo <jeb00o@yahoo.com>
2018-10-26 22:35:49 -0700

    v1.3: add Oreo support

--- app/src/main/java/com/none/hidecarrier_zte/hide_carrier.java
+++ app/src/main/java/com/none/hidecarrier_zte/hide_carrier.java
@@ -1,11 +1,13 @@
 package com.none.hidecarrier_zte;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 
+import android.os.Build;
 import android.view.View;
 import android.widget.TextView;
 
@@ -18,31 +20,42 @@ public class hide_carrier implements IXposedHookLoadPackage {
             return;
         }
 
-        findAndHookMethod("com.android.keyguard.CarrierText", loadPackageParam.classLoader, "updateText", new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                //XposedBridge.log("after updatetext!");
+        if (Build.VERSION.SDK_INT >= 26) {
+            findAndHookMethod("com.android.keyguard.CarrierText", loadPackageParam.classLoader, "updateCarrierText",
+                    new XC_MethodReplacement() {
+                        protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                                    return null;
+                        }
+                    });
+        }
+        else {
+            findAndHookMethod("com.android.keyguard.CarrierText", loadPackageParam.classLoader, "updateText", new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    //XposedBridge.log("after updatetext!");
 
-                Integer mNumPhones = (Integer) XposedHelpers.getObjectField(param.thisObject, "mNumPhones");
-                if (mNumPhones == 0)
-                    return;
+                    Integer mNumPhones = (Integer) XposedHelpers.getObjectField(param.thisObject, "mNumPhones");
+                    if (mNumPhones == 0)
+                        return;
 
-                TextView[] mCarrierName = new TextView[mNumPhones];
-                mCarrierName = (TextView[]) XposedHelpers.getObjectField(param.thisObject, "mCarrierName");
+                    TextView[] mCarrierName = new TextView[mNumPhones];
+                    mCarrierName = (TextView[]) XposedHelpers.getObjectField(param.thisObject, "mCarrierName");
 
-                if (mCarrierName == null)
-                    return;
+                    if (mCarrierName == null)
+                        return;
 
-                //XposedBridge.log("kill carrier(updatetext).");
+                    //XposedBridge.log("kill carrier(updatetext).");
 
-                mCarrierName[0].setVisibility(TextView.GONE);
-                if (mNumPhones == 2)
-                {
-                    //TextView sepTextView = (TextView) XposedHelpers.getObjectField(param.thisObject, "mOperatorSeparator");
-                    //sepTextView.setVisibility(TextView.GONE);
-                    mCarrierName[1].setVisibility(TextView.GONE);
+                    mCarrierName[0].setVisibility(TextView.GONE);
+                    if (mNumPhones == 2)
+                    {
+                        //TextView sepTextView = (TextView) XposedHelpers.getObjectField(param.thisObject, "mOperatorSeparator");
+                        //sepTextView.setVisibility(TextView.GONE);
+                        mCarrierName[1].setVisibility(TextView.GONE);
+                    }
                 }
-            }
-        });
+            });
+        }
     }
 }
+
